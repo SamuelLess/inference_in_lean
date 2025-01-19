ffc2aa3d1238e0964e17f068fff79d1546850ce9
Samuel Leßmann <Samuel@LMpost.de>
2025-01-19 17:00:46 +0100

    Introduce Substitutions and move examples
    - handling fresh_vars is work in progress

--- InferenceInLean/Basic.lean
+++ InferenceInLean/Basic.lean
@@ -11,6 +11,7 @@ set_option autoImplicit false
 - logical connectives (domain-independent)
 -> Boolean combinations, quantifiers
 -/
+namespace FirstOrder
 
 structure Signature where
     funs : Type
@@ -23,17 +24,34 @@ inductive Term (sig : Signature) (X : Variables) where
     | func (f: sig.funs)
            (args: List (Term sig X))
 
+def Term.freeVars {sig : Signature} {X: Variables} : @Term sig X -> Set X
+    | .var x => {x}
+    | .func _ [] => ∅
+    | .func f (a :: args) => Term.freeVars a ∪ Term.freeVars (Term.func f args)
+    -- not using foldl becaus termination...
+
 
 -- TODO: build all of this up with the `ValidTerm`
 inductive Atom (sig : Signature) (X : Type) where
     | pred (p: sig.preds) (args: List (Term sig X))
     | eq (lhs rhs: Term sig X)
 
-inductive Literal (sig: Signature) (X: Variables) where
+def Atom.freeVars {sig : Signature} {X: Variables} : @Atom sig X -> Set X
+    | .pred _ args => args.foldl (λ acc t => acc ∪ Term.freeVars t) ∅
+    | .eq lhs rhs => Term.freeVars lhs ∪ Term.freeVars rhs
+
+inductive Literal (sig : Signature) (X : Variables) where
     | pos (a: @Atom sig X)
     | neg (a: @Atom sig X)
 
+def Literal.comp {sig : Signature} {X : Variables} : Literal sig X -> Literal sig X
+    | .pos a => .neg a
+    | .neg a => .pos a
+
 def Clause (sig: Signature) (X: Variables) := List (Literal sig X)
+
+instance {sig : Signature} {X: Variables} : Membership (Literal sig X) (Clause sig X)
+    := List.instMembership
 
 inductive Formula (sig: Signature) (X: Variables) where
     | falsum
@@ -46,39 +64,6 @@ inductive Formula (sig: Signature) (X: Variables) where
     | iff (f g: @Formula sig X)
     | all (x: X) (f: @Formula sig X)
     | ex (x: X) (f: @Formula sig X)
-
-
-/-
-### Example Peano Arithmetic
--/
-
-inductive PeanoFuns where
-    | zero
-    | succ
-    | add
-    | mul
-
-inductive PeanoPreds where
-    | less_than
-
-@[simp]
-def ex_sig_peano : Signature := ⟨PeanoFuns, PeanoPreds⟩
-
-/-
-> ∀x, y ((x < y ∨ x ≈ y) ↔ ∃z (x + z ≈ y))
--/
-@[simp]
-def ex_formula_peano : @Formula ex_sig_peano String :=
-    .all "x" (.all "y" (.iff
-        (.or
-            (.atom (.pred .less_than [.var "x", .var "y"]))
-            (.atom (.eq (.var "x") (.var "y")))
-        )
-        (.ex "z"
-            (.atom (.eq (.func .add [.var "x", .var "z"]) (.var "y")))
-        )
-    ))
-
 
 def Substitution (sig : Signature) (X : Variables) := X → Term sig X
 
@@ -116,19 +101,6 @@ structure Interpretation (sig: Signature) where
     functions : sig.funs -> (List univ -> univ)
     predicates : sig.preds -> (List univ -> Prop)
 
-@[simp]
-def ex_interpret_peano : Interpretation ex_sig_peano :=
-    {
-      univ := ℕ
-      functions := λ
-        | .zero => λ _ => 0
-        | .succ => λ xs => xs[0]! + 1
-        | .add => λ xs => xs[0]! + xs[1]!
-        | .mul => λ xs => xs[0]! * xs[1]!
-      predicates := λ f => match f with
-        | .less_than => λ xs => xs[0]! < xs[1]!
-    }
-
 /-
 ### Assigments
 > β : X → univ
@@ -141,12 +113,6 @@ def Assignment (X: Variables) (univ: Universes) := X → univ
 def Assignment.modify {X: Variables} {univ: Universes} [BEq X]
                       (β: Assignment X univ) (x: X) (a: univ) : Assignment X univ :=
     λ y => if y == x then a else β y
-
-def ex_assig_peano : Assignment String Nat
-    | "x" => 21 -- this is kind of a shortcut for `s ... s zero`
-    | "y" => 42 -- this is kind of a shortcut for `s zero`
-    | _ => 0
-
 
 mutual -- this is just to persuade lean of the termination
     @[simp]
@@ -161,19 +127,11 @@ mutual -- this is just to persuade lean of the termination
         | x :: xs => (evalTerm I β x) :: eval_subterm I β xs
 end
 
-#eval @evalTerm ex_sig_peano String ex_interpret_peano ex_assig_peano (Term.var "y")
-
-def ex_term_peano : Term ex_sig_peano String :=
-    Term.func .add [Term.var "x", Term.var "y"]
-
-#eval @evalTerm ex_sig_peano String ex_interpret_peano ex_assig_peano ex_term_peano
-
 @[simp]
 def substituteAtom {sig : Signature} {X: Variables}
                    (σ: Substitution sig X) : @Atom sig X -> Atom sig X
     | Atom.pred p args => Atom.pred p $ args.map (substituteTerm σ)
     | Atom.eq lhs rhs => Atom.eq (substituteTerm σ lhs) (substituteTerm σ rhs)
-
 
 @[simp]
 def evalAtom {sig: Signature} {X: Variables}
@@ -195,33 +153,26 @@ def evalFormula {sig: Signature} {X: Variables} [BEq X]
     | Formula.all x f => ∀ a : I.univ, evalFormula I (β.modify x a) f
     | Formula.ex x f => ∃ a : I.univ, evalFormula I (β.modify x a) f
 
-lemma ex_peano_proof: @evalFormula ex_sig_peano String (@instBEqOfDecidableEq String instDecidableEqString) ex_interpret_peano ex_assig_peano ex_formula_peano := by
-    simp
-    intro a b
-    apply Iff.intro
-    · intro h
-      cases h
-      next h' => use b - a
-                 refine Nat.add_sub_of_le ?_
-                 exact h'.le
-      · use 0; assumption
-    · intro h
-      obtain ⟨z, hz⟩ := h
-      cases hz
-      simp only [Nat.lt_add_right_iff_pos, Nat.self_eq_add_right]
-      exact Or.symm (Nat.eq_zero_or_pos z)
 
-def exa : Formula ex_sig_peano String := .and .falsum .verum
-example : ¬@evalFormula ex_sig_peano _ _ ex_interpret_peano ex_assig_peano exa :=
-    of_eq_true (Eq.trans (congrArg Not (and_true False)) not_false_eq_true)
+def Formula.freeVars {sig : Signature} {X: Variables} : @Formula sig X -> Set X
+    | Formula.falsum => ∅
+    | Formula.verum => ∅
+    | Formula.atom a => Atom.freeVars a
+    | Formula.neg f => Formula.freeVars f
+    | Formula.and f g => Formula.freeVars f ∪ Formula.freeVars g
+    | Formula.or f g => Formula.freeVars f ∪ Formula.freeVars g
+    | Formula.imp f g => Formula.freeVars f ∪ Formula.freeVars g
+    | Formula.iff f g => Formula.freeVars f ∪ Formula.freeVars g
+    | Formula.all x f => Formula.freeVars f \ {x}
+    | Formula.ex x f => Formula.freeVars f \ {x}
 
 /-
 TODO: take care of bound variables in quantifiers
-(Qx F)σ = Qz (F σ[x 7→ z]) with z a fresh variable and Q ∈ {∀, ∃}
+(Qx F)σ = Qz (F σ[x → z]) with z a fresh variable and Q ∈ {∀, ∃}
 However, how can we cleanly ensure that z is fresh?
 -/
 @[simp]
-def substituteFormula {sig : Signature} {X: Variables}
+noncomputable def substituteFormula {sig : Signature} {X: Variables} [inst : BEq X] [inst_nonempty : Nonempty X]
                       (σ: Substitution sig X) : @Formula sig X -> @Formula sig X
     | Formula.falsum => Formula.falsum
     | Formula.verum => Formula.verum
@@ -231,8 +182,11 @@ def substituteFormula {sig : Signature} {X: Variables}
     | Formula.or f g => Formula.or (substituteFormula σ f) (substituteFormula σ g)
     | Formula.imp f g => Formula.imp (substituteFormula σ f) (substituteFormula σ g)
     | Formula.iff f g => Formula.iff (substituteFormula σ f) (substituteFormula σ g)
-    | Formula.all x f => Formula.all x (substituteFormula (σ) f)
-    | Formula.ex x f => Formula.ex x (substituteFormula (σ) f)
+    | Formula.all x f => have z := Classical.choice inst_nonempty
+                         Formula.all z (substituteFormula (σ.modify sig X x (Term.var z)) f)
+    | Formula.ex x f => have z := Classical.choice inst_nonempty
+                        Formula.ex x (substituteFormula (σ.modify sig X x (Term.var z)) f)
+
 
 
 /-
@@ -246,10 +200,6 @@ def substituteFormula {sig : Signature} {X: Variables}
 def EntailsInterpret {sig : Signature} {X: Variables} [BEq X]
             (I : @Interpretation sig) (β : Assignment X I.univ) (F : @Formula sig X) : Prop :=
     evalFormula I β F
-
-example : @EntailsInterpret ex_sig_peano String _ ex_interpret_peano
-            ex_assig_peano ex_formula_peano := by
-            exact ex_peano_proof
 
 theorem not_entails_not {sig : Signature} {X: Variables} [BEq X]
             (I : @Interpretation sig) (β : Assignment X I.univ) (F : @Formula sig X) :
@@ -300,32 +250,15 @@ def Unsatisfiable {sig : Signature} {X: Variables} [BEq X]
                 (F : @Formula sig X) : Prop := ¬Satisfiable F
 
 /-
-Proposition 3.3.3 Let F and G be formulas, let N be a set of formulas. Then
-(i) F is valid if and only if ¬F is unsatisfiable.
-(ii) F |= G if and only if F ∧ ¬G is unsatisfiable.
--/
-
-theorem valid_iff_not_unsat
-    {sig : Signature} {X : Variables} [inst : BEq X] (F : Formula sig X) :
-    Valid F ↔ @Unsatisfiable sig X inst (Formula.neg F) := by
-        simp
-
-theorem entails_iff_and_not_unsat
-    {sig : Signature} {X : Variables} [inst : BEq X] (F G : Formula sig X) :
-    Entails F G ↔ @Unsatisfiable sig X inst (Formula.and F (Formula.neg G)) := by
-        simp
-
-/-
 Expand this to sets of Formulas
 -/
-
 @[simp]
 def SetEntails {sig : Signature} {X: Variables} [BEq X]
             (N : Set (Formula sig X)) (F : Formula sig X) : Prop :=
     ∀ (I : @Interpretation sig) (β : Assignment X I.univ),
         (∀ G ∈ N, EntailsInterpret I β G) → EntailsInterpret I β F
 
-theorem entails_setEntails
+lemma entails_setEntails
     {sig : Signature} {X : Variables} [inst : BEq X] (F G : Formula sig X) :
     Entails F G ↔ @SetEntails sig X inst {F} G := by
         simp
@@ -352,23 +285,30 @@ lemma unsat_as_set_unsat
     {sig : Signature} {X : Variables} [inst : BEq X] (F : Formula sig X) :
        Unsatisfiable F → @SetUnsatisfiable sig X inst {F} :=
        by simp
-/-
--- TODO: finish this proof
-(iii) N |= G if and only if N ∪ {¬G} is unsatisfiable.
--/
-theorem setEntails_iff_union_not_unsat
-    {sig : Signature} {X : Variables} [inst : BEq X] (N : Set $ Formula sig X) (G : Formula sig X) :
-    SetEntails N G ↔ @SetUnsatisfiable sig X inst (N ∪ {Formula.neg G}) := by
-        sorry
 
+/-Expand this to Literals and Clauses-/
+def Literal.satisfied_by {sig : Signature} {X: Variables} [BEq X]
+                         (L : Literal sig X) (I : @Interpretation sig)
+                         (β : Assignment X I.univ) : Prop :=
+        EntailsInterpret I β (match L with
+            | Literal.pos a => Formula.atom a
+            | Literal.neg a => Formula.neg (Formula.atom a)
+        )
+
+def ClauseSatisfiable {sig : Signature} {X: Variables} [BEq X]
+                      (C : Clause sig X) : Prop :=
+    ∃ (I : @Interpretation sig) (β : Assignment X I.univ),
+        ∃ L : Literal sig X, L ∈ C ∧ L.satisfied_by I β
+
+def ClauseSetSatisfiable {sig : Signature} {X : Variables} [BEq X]
+                         (N : Set $ Clause sig X) : Prop :=
+    ∃ (I : @Interpretation sig) (β : Assignment X I.univ),
+        ∀ C ∈ N, ∃ L, L ∈ C ∧ L.satisfied_by I β
 
 /-
 ### 3.3.4 Substitution Lemma
 - do it later maybe
 -/
-
-
-
 
 
 /-
---
+++ InferenceInLean/Exercises.lean
@@ -0,0 +1,148 @@
+import InferenceInLean.Basic
+
+open FirstOrder
+
+/-
+### Example Peano Arithmetic
+-/
+
+inductive PeanoFuns where
+    | zero
+    | succ
+    | add
+    | mul
+
+inductive PeanoPreds where
+    | less_than
+
+@[simp]
+def ex_sig_peano : Signature := ⟨PeanoFuns, PeanoPreds⟩
+
+/-
+> ∀x, y ((x < y ∨ x ≈ y) ↔ ∃z (x + z ≈ y))
+-/
+@[simp]
+def ex_formula_peano : @Formula ex_sig_peano String :=
+    .all "x" (.all "y" (.iff
+        (.or
+            (.atom (.pred .less_than [.var "x", .var "y"]))
+            (.atom (.eq (.var "x") (.var "y")))
+        )
+        (.ex "z"
+            (.atom (.eq (.func .add [.var "x", .var "z"]) (.var "y")))
+        )
+    ))
+
+@[simp]
+def ex_interpret_peano : Interpretation ex_sig_peano :=
+    {
+      univ := ℕ
+      functions := λ
+        | .zero => λ _ => 0
+        | .succ => λ xs => xs[0]! + 1
+        | .add => λ xs => xs[0]! + xs[1]!
+        | .mul => λ xs => xs[0]! * xs[1]!
+      predicates := λ f => match f with
+        | .less_than => λ xs => xs[0]! < xs[1]!
+    }
+
+def ex_assig_peano : Assignment String Nat
+    | "x" => 21 -- this is kind of a shortcut for `s ... s zero`
+    | "y" => 42 -- this is kind of a shortcut for `s zero`
+    | _ => 0
+
+
+#eval @evalTerm ex_sig_peano String ex_interpret_peano ex_assig_peano (Term.var "y")
+
+def ex_term_peano : Term ex_sig_peano String :=
+    Term.func .add [Term.var "x", Term.var "y"]
+
+#eval @evalTerm ex_sig_peano String ex_interpret_peano ex_assig_peano ex_term_peano
+
+lemma ex_peano_proof: @evalFormula ex_sig_peano String (@instBEqOfDecidableEq String instDecidableEqString) ex_interpret_peano ex_assig_peano ex_formula_peano := by
+    simp
+    intro a b
+    apply Iff.intro
+    · intro h
+      cases h
+      next h' => use b - a
+                 refine Nat.add_sub_of_le ?_
+                 exact h'.le
+      · use 0; assumption
+    · intro h
+      obtain ⟨z, hz⟩ := h
+      cases hz
+      simp only [Nat.lt_add_right_iff_pos, Nat.self_eq_add_right]
+      exact Or.symm (Nat.eq_zero_or_pos z)
+
+def exa : Formula ex_sig_peano String := .and .falsum .verum
+example : ¬@evalFormula ex_sig_peano _ _ ex_interpret_peano ex_assig_peano exa :=
+    of_eq_true (Eq.trans (congrArg Not (and_true False)) not_false_eq_true)
+
+
+example : @EntailsInterpret ex_sig_peano String _ ex_interpret_peano
+            ex_assig_peano ex_formula_peano := by
+            exact ex_peano_proof
+
+
+/-
+Proposition 3.3.3 Let F and G be formulas, let N be a set of formulas. Then
+(i) F is valid if and only if ¬F is unsatisfiable.
+(ii) F |= G if and only if F ∧ ¬G is unsatisfiable.
+-/
+theorem valid_iff_not_unsat
+    {sig : Signature} {X : Variables} [inst : BEq X] (F : Formula sig X) :
+    Valid F ↔ @Unsatisfiable sig X inst (Formula.neg F) := by
+        simp
+
+theorem entails_iff_and_not_unsat
+    {sig : Signature} {X : Variables} [inst : BEq X] (F G : Formula sig X) :
+    Entails F G ↔ @Unsatisfiable sig X inst (Formula.and F (Formula.neg G)) := by
+        simp
+
+/-
+-- TODO: finish this proof
+(iii) N |= G if and only if N ∪ {¬G} is unsatisfiable.
+-/
+theorem setEntails_iff_union_not_unsat
+    {sig : Signature} {X : Variables} [inst : BEq X] (N : Set $ Formula sig X) (G : Formula sig X) :
+    SetEntails N G ↔ @SetUnsatisfiable sig X inst (N ∪ {Formula.neg G}) := by
+        sorry
+
+
+/-
+Exercise 4.7 (*)
+Let Π be a set of propositional variables. Let N and N' be sets
+of clauses over Π. Let S be a set of literals that does not contain any complementary
+literals. Prove: If every clause in N contains at least one literal L with L ∈ S and if no
+clause in N' contains a literal L with L ∈ S, then N ∪ N' is satisfiable if and only if N'
+is satisfiable.
+-/
+theorem ex_4_7 {sig : Signature} {X : Variables} [BEq X]
+    (N N' : Set $ Clause sig X) (S : Set $ Literal sig X)
+    (hSnoCompl : ∀ L ∈ S, L.comp ∉ S)
+    (hNsatByS : ∀ C ∈ N, ∃ L ∈ C, L ∈ S) (hN'noComplS : ∀ C ∈ N', ∀ L ∈ C, L ∉ S) :
+    (ClauseSetSatisfiable (N ∪ N') ↔ ClauseSetSatisfiable N') := by
+        apply Iff.intro
+        · simp [ClauseSetSatisfiable]
+          intro I β
+          intro h
+          apply Exists.intro
+          · apply Exists.intro
+            · intro C a
+              apply h
+              simp_all only [or_true]
+        · simp [ClauseSetSatisfiable]
+          intro I_N' β_N'
+          intro hN'sat
+          use I_N'
+          use β_N'
+          intro C hC
+          cases hC
+          /- This is the actual hard case of this exercise. On paper it might look like this:
+             - Show that I_N' and β_N' do not contradict (SAT N) (this is due to hN'noComplS)
+             - Expand β_N' by the assignments implied by S to β_N'andS
+             - then β_N'andS satisfies N using hNsatByS
+          -/
+          next h => sorry
+          next h => exact hN'sat C h
